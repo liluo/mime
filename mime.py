a632d0843370db8c373e6c405db6671e9f4e85fd
liluo <i@liluo.org>
2013-08-14 10:30:14 +0800

    register mime

---
+++ mime.py
@@ -0,0 +1,2 @@
+def io():
+    print 'todo'
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='mime',
+      version='0.0.1',
+      keywords='mime types',
+      description='mime types',
+      license='MIT License',
+      url='https://github.com/liluo/mime',
+      author='liluo',
+      author_email='i@liluo.org',
+
+      py_modules=['mime'],
+      include_package_data=True,
+      platforms='any',
+      install_requires=[])
